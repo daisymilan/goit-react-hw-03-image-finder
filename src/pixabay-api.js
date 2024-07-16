afa25fe6a8cccf40aa018bbfe80c93bdadf0d642
daisymilan <daisymilan3@gmail.com>
2024-07-16 23:34:11 +0800

    update homework

--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '41258265-293e4bea479457c1d7a454ddf';
+const API_KEY = '43106887-eff60e26b4e1d2e0df5166367';
 
 export const getAPI = async (query, page) => {
   const url = `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
